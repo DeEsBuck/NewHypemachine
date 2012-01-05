ccf032279ae77946cc1195fce8f78fcf4bcebb6d
Tim Czubayko <tc@meetive.com>
2012-01-05 11:14:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewHypemachine::Application.config.secret_token = '669cb06618416fc4f085b6fe7aa6d476c66975a2dea5215193a2548eb646a6914ddde41b62ba40208b126a1007d3bee2ce3ecd9d226f20ce3592a0ae6ce4076b'
